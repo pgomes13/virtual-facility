59627f2f0c26c1e1f0b700967cddcc79f3cffd4c
P Gomes <pritgomes@gmail.com>
2026-02-20 12:43:56 +1000

    feat: added rabbitmq mannual ack

--- apps/alarms-service/src/alarms-service.module.ts
+++ apps/alarms-service/src/alarms-service.module.ts
@@ -1,9 +1,29 @@
 import { Module } from '@nestjs/common';
 import { AlarmsServiceController } from './alarms-service.controller';
 import { AlarmsServiceService } from './alarms-service.service';
+import { ClientsModule, Transport } from '@nestjs/microservices';
+import { NATS_MESSAGE_BROKER, NOTIFICATIONS_SERVICE } from './constants';
 
 @Module({
-  imports: [],
+  imports: [
+    ClientsModule.register([
+      {
+        name: NATS_MESSAGE_BROKER,
+        transport: Transport.NATS,
+        options: {
+          servers: process.env.NATS_URL,
+        },
+      },
+      {
+        name: NOTIFICATIONS_SERVICE,
+        transport: Transport.RMQ,
+        options: {
+          urls: [process.env.RABBITMQ_URL!],
+          queue: 'notifications-service',
+        },
+      },
+    ]),
+  ],
   controllers: [AlarmsServiceController],
   providers: [AlarmsServiceService],
 })
